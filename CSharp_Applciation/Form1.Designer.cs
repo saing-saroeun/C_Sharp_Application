f188a2653bd7fdac910671709db8c121f6b3e965
GURUTECH <saingsaroeuninfo@gmail.com>
2024-07-30 20:38:10 +0700

    First Deploy

--- CSharp_Applciation/Form1.Designer.cs
+++ CSharp_Applciation/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(846, 408);
+            this.ClientSize = new System.Drawing.Size(1310, 441);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
